4edf297bfc540ed571daf6e5f0ef5561e79350cb
ABHISHEK SHARMA <abhisheksharma@ABHISHEKs-MacBook-Air.local>
2023-07-09 00:38:30 +0530

    uncomment

--- app/resources/upload_sales_order.py
+++ app/resources/upload_sales_order.py
@@ -98,23 +98,23 @@ class UploadSalesOrderResource(Resource):
                 return {'status' : 200, 'message' : 'No order file given'}
             is_file_saved = self.save_file(order_file)
 
-            # is_folder_created = self.create_s3_folder(self.get_today_date())
-            # s3_file_name = is_folder_created + is_file_saved.split('/')[1]
-            # if is_file_saved:
-            #     is_uploaded = self.upload_file(is_file_saved,s3_file_name)
-            #     if is_uploaded:
-            #         is_order_stored = self.process_client.save_order(is_file_saved)
-            #         if is_order_stored['status']:
-            #             ret_obj['status'] = 200
-            #             ret_obj['message'] = f"Order created successfully for {is_file_saved}"
-            #             ret_obj['order_id'] = is_order_stored['order_id']
-            #         else:
-            #             self.logger.log_error(f"MAIN-Error while storing data in DB for {order_file}")
-            #     else:
-            #         self.logger.log_error(f"MAIN-Error while uploading order file to S3 : {order_file}")
-            # else:
-            #     self.logger.log_error(f"MAIN-Error while saving order file : {order_file}")
-            # return ret_obj, 201
+            is_folder_created = self.create_s3_folder(self.get_today_date())
+            s3_file_name = is_folder_created + is_file_saved.split('/')[1]
+            if is_file_saved:
+                is_uploaded = self.upload_file(is_file_saved,s3_file_name)
+                if is_uploaded:
+                    is_order_stored = self.process_client.save_order(is_file_saved)
+                    if is_order_stored['status']:
+                        ret_obj['status'] = 200
+                        ret_obj['message'] = f"Order created successfully for {is_file_saved}"
+                        ret_obj['order_id'] = is_order_stored['order_id']
+                    else:
+                        self.logger.log_error(f"MAIN-Error while storing data in DB for {order_file}")
+                else:
+                    self.logger.log_error(f"MAIN-Error while uploading order file to S3 : {order_file}")
+            else:
+                self.logger.log_error(f"MAIN-Error while saving order file : {order_file}")
+            return ret_obj, 201
         except Exception as e:
             self.logger.log_error(f"MAIN-Error while uploading Sales order : {e}")
             return ret_obj, 500
