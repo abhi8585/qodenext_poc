7db45523b53fc2bf96a6e5d22557b2501c33e3c9
abhi8585 <abhi.sharma1114@gmail.com>
2023-07-19 10:24:03 +0530

    adding constructor

--- app/resources/access_management/login.py
+++ app/resources/access_management/login.py
@@ -6,16 +6,14 @@ from app.utils.logger.logger_client import LoggerClient
 from app.utils.logger.logger_verbose import VerboseLevels
 from app.utils.uuid.uuid_client import UUIDClient
 import hashlib
-import jwt
-
-
-config = ConfigClient(env='dev')
-mysql_uri = config.get_value("Database", "uri")
-mysql_client = MySQLClient(mysql_uri)
-logger = LoggerClient(verbosity=VerboseLevels.INFO.value)
 
 
 class LoginResource(Resource):
+
+    def __init__(self):
+        self.config = ConfigClient(env='dev')
+        self.mysql_client = MySQLClient(self.config.get_value("Database", "uri"))
+        self.logger = LoggerClient(verbosity=VerboseLevels.INFO.value)
 
     @staticmethod
     def decrypt_password(plain_password, hashed_password):
@@ -39,24 +37,23 @@ class LoginResource(Resource):
 
             if not email or not password:
                 return {'message': 'Email and password are required.'}, 400
-            user = mysql_client.select('user', filter_condition=f"WHERE user_email = '{email}'")
+            user = self.mysql_client.select('user', filter_condition=f"WHERE user_email = '{email}'")
             if not user:
                 return {'message': 'User not found.'}, 404
             if len(user['results']) > 0:
                 role_name = None
                 user_id = user['results'][0]['user_id']
                 user_name = user['results'][0]['user_name']
-                print(user_name)
-                role_id = mysql_client.select(table_name='user_to_role',filter_condition=f"where user_id = {user_id}")
+                role_id = self.mysql_client.select(table_name='user_to_role',filter_condition=f"where user_id = {user_id}")
                 if role_id:
                     if len(role_id['results']) > 0:
                         role_id = role_id['results'][0]['role_id']
-                        role_name = mysql_client.select(table_name='role',filter_condition=f"where role_id = {role_id}")
+                        role_name = self.mysql_client.select(table_name='role',filter_condition=f"where role_id = {role_id}")
                         if role_name:
                             if len(role_name) > 0:
                                 role_name = role_name['results'][0]['role_name']
                         else:
-                            print(f"role name is empty")
+                            self.logger.log_info(f"no role name is assigned")
                 else:
                     role_name = ''
                 encrypted_password = user['results'][0]['user_password']
@@ -64,15 +61,15 @@ class LoginResource(Resource):
 
                 if decrypted_password:
                     auth_token = self.generate_auth_token(email)
-                    mysql_client.insert(table_name='access_token', column_values={'access_token' :auth_token})
-                    logger.log_info(f"User login successfully!")
+                    self.mysql_client.insert(table_name='access_token', column_values={'access_token' :auth_token})
+                    self.logger.log_info(f"User login successfully!")
                     return {"user_name":user_name,"user_id":user_id,'status':200,'message': 'Login successfull.','access_token':auth_token, 'role_name':role_name}, 200
                 else:
-                    logger.log_info(f"Invalid password!")
+                    self.logger.log_info(f"Invalid password!")
                     return {'message': 'Invalid password.'}, 401
             else:
                 return {'message':'User not found'}, 404
 
         except Exception as e:
-            logger.log_error(f"An error occurred during login: {str(e)}")
+            self.logger.log_error(f"An error occurred during login: {str(e)}")
             return {'message': 'An error occurred during login.'}, 500
