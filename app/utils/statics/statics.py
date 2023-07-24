b283d791e4f6c9a6ab49f80a9c61d51d1ad3d905
abhi8585 <abhi.sharma1114@gmail.com>
2023-07-24 18:07:06 +0530

    adding static module
    adding static module

---
+++ app/utils/statics/statics.py
@@ -0,0 +1,12 @@
+from enum import Enum
+class StaticValues(Enum):
+    ENV_DEV = "dev"
+    SMTP_SERVER = "smtppro.zoho.in"
+    EMAIL_PORT = 465
+    CUSTOMER = 'customer'
+    WAREHOUSE = 'warehouse'
+    BREWERY = 'brewery'
+    DISPATCHED = 'dispatched'
+    DELIVERED = 'delivered'
+    
+
