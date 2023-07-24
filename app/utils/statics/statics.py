c8e44d890e5f3bd9307f1665352a12635fc4b478
abhi8585 <abhi.sharma1114@gmail.com>
2023-07-25 02:05:46 +0530

    new static values

--- app/utils/statics/statics.py
+++ app/utils/statics/statics.py
@@ -8,5 +8,11 @@ class StaticValues(Enum):
     BREWERY = 'brewery'
     DISPATCHED = 'dispatched'
     DELIVERED = 'delivered'
+    RECEIVED = 'received'
+    CUSTOMER_DISPATCH_FROM_WAREHOUSE = 'cdispatch'
+    BREWERY_DISPATCH_FROM_WAREHOUSE = 'bdispatch'
+    WAREHOUSE_TO_CUSTOMER = 'wtc'
+    WAREHOUSE_TO_BREWERY = 'wtb'
+    PICKED = 'picked'
     
 
