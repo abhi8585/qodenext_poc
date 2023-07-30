e001f827ac929e1ea72803d7c8cbb239b7497eb4
abhi8585 <abhi.sharma1114@gmail.com>
2023-07-30 22:11:30 +0530

    updating logger client for logs in folder
    updating logger client for logs in folder

--- app/utils/logger/logger_client.py
+++ app/utils/logger/logger_client.py
@@ -1,20 +1,66 @@
+# import logging
+
+# class LoggerClient:
+#     _instance = None
+
+#     def __new__(cls, verbosity):
+#         if cls._instance is None:
+#             cls._instance = super().__new__(cls)
+#             cls._instance.logger = logging.getLogger(__name__)
+#             cls._instance.logger.setLevel(verbosity)
+#             console_handler = logging.StreamHandler()
+#             console_handler.setLevel(verbosity)
+#             formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+#             console_handler.setFormatter(formatter)
+#             cls._instance.logger.addHandler(console_handler)
+#         return cls._instance
+
+#     def log_info(self, message):
+#         self.logger.info(message)
+
+#     def log_error(self, message):
+#         self.logger.error(message)
+
+#     def log_warning(self, message):
+#         self.logger.warning(message)
+
+
+
 import logging
+import os
+import datetime
 
 class LoggerClient:
-    def __init__(self, verbosity):
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(verbosity)
+    _instance = None
 
-        # Create a console handler
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(verbosity)
+    def __new__(cls, verbosity):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.logger = logging.getLogger(__name__)
+            cls._instance.logger.setLevel(verbosity)
 
-        # Create a formatter and add it to the console handler
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        console_handler.setFormatter(formatter)
+            # Create the "logs" folder if it doesn't exist
+            logs_folder = os.path.join(os.path.dirname(__file__), "logs")
+            if not os.path.exists(logs_folder):
+                os.mkdir(logs_folder)
 
-        # Add the console handler to the logger
-        self.logger.addHandler(console_handler)
+            # Set up the file handler to write logs to the date-specific text file
+            current_date = datetime.date.today().strftime("%Y-%m-%d")
+            log_file_path = os.path.join(logs_folder, f"{current_date}.txt")
+            file_handler = logging.FileHandler(log_file_path)
+            file_handler.setLevel(verbosity)
+
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            file_handler.setFormatter(formatter)
+            cls._instance.logger.addHandler(file_handler)
+
+            # Set up the console handler for displaying logs on the console
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(verbosity)
+            console_handler.setFormatter(formatter)
+            cls._instance.logger.addHandler(console_handler)
+
+        return cls._instance
 
     def log_info(self, message):
         self.logger.info(message)
@@ -24,4 +70,3 @@ class LoggerClient:
 
     def log_warning(self, message):
         self.logger.warning(message)
-
